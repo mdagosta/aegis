4af5b0b85b68661d468a079c6eb6b6c0579fe9d1
Michael D'Agosta <mdagosta@codebug.com>
2019-09-29 06:13:00 +0000

    Trying to get version number to work in setup.py

--- aegis/tmpl/setup.py
+++ aegis/tmpl/setup.py
@@ -3,6 +3,8 @@
 """
 Build {{app_name}} inside of a virtualenv.
 virtualenv --python=`which python3` --distribute --system-site-packages virtualenv
+. ./virtualenv/bin/activate
+python setup.py develop
 """
 
 import os
@@ -15,11 +17,11 @@ setuptools.setup (
     author = "",
     author_email = '',
     url = '',
-    install_requires = [
+    setup_requires = [
         'tornado == 4.5.2',
         'psycopg2 == 2.7.3.2',
         'aegis == 0.0.1',
     ],
-    dependency_links=['https://bitbucket.org/mdagosta/aegis/get/rough-draft.tar.gz'],
+    dependency_links=['https://bitbucket.org/mdagosta/aegis/get/rough-draft.tar.gz#egg=aegis-0.0.1'],
     package_data = { '': ['templates/*', 'static/*/*', 'sql/*'] },
 )
--- setup.py
+++ setup.py
@@ -17,4 +17,12 @@ setuptools.setup (
     author = "Michael D'Agosta",
     author_email = 'mdagosta@codebug.com',
     url = 'https://bitbucket.org/mdagosta/aegis',
+    packages = ['aegis'],
+    #entry_points={
+    #    'console_scripts': [
+    #        'rst2pdf = project_a.tools.pdfgen [PDF]',
+    #        'rst2html = project_a.tools.htmlgen',
+    #        # more script entry points ...
+    #    ],
+    #}
 )

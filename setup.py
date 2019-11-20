9e51bab13e89ae007659c17a646585b3ef629b00
Michael D'Agosta <mdagosta@codebug.com>
2019-11-20 01:24:45 +0000

    Small features surrounding PyPi packaging.

--- setup.py
+++ setup.py
@@ -1,23 +1,36 @@
 #!/usr/bin/env python3
 
 """
-Minimal packaging setup to use command line tools to get started
+Minimal packaging setup to use command line tools to get started after checking out the repository:
 virtualenv --python=`which python3` virtualenv
 . ./virtualenv/bin/activate
 python setup.py develop
+
+To build aegis for PyPi:
+
+Increment the version # ... should this be tied to git tag? `aegis release` ?
+python3 setup.py sdist bdist_wheel
+python3 -m twine upload dist/*
 """
 
 import os
 import setuptools
 
 setuptools.setup (
-    name = 'aegis',
+    name = 'aegis-tools',
     version = '0.0.1',
     description = 'Aegis is a set of battle-tested tools and tricks to help everyone make better software',
+    long_description = 'A combination of tools and framework, Aegis has multiple different uses. You can import it and use the thoroughly made and tested functions. You can use it as a natural extension for the tornado web framework. And you can use it to quickly create a new web application with the structure already built-in, and follow along.'
     author = "Michael D'Agosta",
     author_email = 'mdagosta@codebug.com',
     url = 'https://bitbucket.org/mdagosta/aegis',
+    python_requires='>=3.6',
     packages = ['aegis'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     entry_points={
         'console_scripts': [
             "aegis = aegis.aegis:main",

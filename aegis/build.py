6a4e7e4cf9d19a16bf1602f6d514055597284435
Michael D'Agosta <mdagosta@codebug.com>
2025-05-09 22:41:59 +0000

    Blind attempt to run build

--- aegis/build.py
+++ aegis/build.py
@@ -118,6 +118,18 @@ class Build:
                 else:
                     if self._shell_exec("npm run %s" % self.build_row['env'], cwd=self.build_dir, build_step='build'):
                         return
+
+            # If options.build_vite_dirs, run through them running npm run build
+            build_vite_dirs = aegis.config.get('build_vite_dirs')
+            if build_vite_dirs:
+                aegis.stdlib.logw(build_vite_dirs, "BUILD_VITE_DIRS")
+                for build_vite_dir in build_vite_dirs:
+                    build_vite_path = os.path.join(self.build_dir, build_vite_dir)
+                    aegis.stdlib.logw(build_vite_path, "BUILD_VITE_PATH")
+                    if self._shell_exec("npm run build", cwd=build_vite_path, build_step='build'):
+                        logging.warning("SUCCESSFUL BUILD... ?!?")
+                        return
+
             # Set up and run yarn if it's installed TODO and yarn.lock file exists in the root of repository
             yarn_lock = os.path.exists(os.path.join(self.build_dir, 'yarn.lock'))
             self.yarn, stderr, exit_status = aegis.stdlib.shell('which yarn', cwd=self.src_dir)
